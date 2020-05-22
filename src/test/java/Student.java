9eef35b6be94e2bc95e433030e54746d5e464bf6
Piotr Szeląg <szelonger@gmail.com>
2020-05-22 14:21:47 +0200

    Konstruktor class added, Student class modified.

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -6,10 +6,20 @@ public class Student {
 
     public String grupa;
 
+    public Student() {
+        System.out.println("Cześć! Witaj w Konstruktorze.");
+    }
+
     public void przedstawSie() {
         System.out.println("Cześć! Nazywam się "  + imie + " " + nazwisko);
     }
 
+    public Student(String noweImie, String noweNazwisko, String nowaGrupa) {
+        imie= noweImie;
+        nazwisko= noweNazwisko;
+        grupa= nowaGrupa;
 
+
+    }
 
 }
