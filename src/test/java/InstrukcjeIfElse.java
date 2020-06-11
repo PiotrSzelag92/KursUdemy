e1452329fee490bd9e09105a5a27b3c385faf0ca
Piotr Szeląg <szelonger@gmail.com>
2020-06-11 15:01:38 +0200

    InstrukcjeIfElse class modified, Petlafor class added

--- src/test/java/InstrukcjeIfElse.java
+++ src/test/java/InstrukcjeIfElse.java
@@ -9,6 +9,10 @@ public class InstrukcjeIfElse {
         sprawdzWiek(20);
         sprawdzWiek(18);
 
+        idzDoPracy(16);
+        idzDoPracy(22);
+        idzDoPracy(67);
+
     }
     public void sprawdzWiek(int wiek){
         if (wiek < 18){
@@ -17,5 +21,14 @@ public class InstrukcjeIfElse {
             System.out.println("Masz wystarczająco dużo lat, aby kupić alkohol");
         }
 
+    }
+    public void idzDoPracy(int wiek){
+        if (wiek <= 18) {
+            System.out.println("Jestem za młody na pracę.");
+        } else if (wiek <= 65){
+            System.out.println("Zapraszamy do pracy.");
+        } else if (wiek > 65){
+            System.out.println("Jesteś już na emeryturze.");
+        }
     }
 }
