49fde60d98eabe385a0ad6cf4401ff2eef1fcc50
Piotr Szeląg <szelonger@gmail.com>
2020-06-11 16:36:34 +0200

    interfejs/Auto added, interfejs/PodstawoweAuto class added, interfejs/interfejsTest added.

---
+++ src/test/java/interfejs/Auto.java
@@ -0,0 +1,10 @@
+package interfejs;
+
+public interface Auto {
+    //kontrakt, który musi być wypełniony przez klasy, które ten interfejs implementują
+
+    public void jedź();
+    public void zatrzymaj();
+    public void otwórzszybę();
+    public void zmieńbieg();
+}
